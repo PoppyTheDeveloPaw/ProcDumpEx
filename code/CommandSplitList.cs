ff97daebc34a77a4d56833180c8fad2d3de246f9
Alexander Peipp <alexander@peipp.org>
2024-04-22 21:23:06 +0200

    Suggestions for improvement built in [issue:86]

--- code/CommandSplitList.cs
+++ code/CommandSplitList.cs
@@ -1,62 +1,89 @@
-﻿namespace ProcDumpEx
+﻿using System.Text;
+
+namespace ProcDumpEx
 {
 	internal class CommandSplitList : List<(bool IsOption, object Value)>
 	{
 		/// <summary>
-		/// Searches for the specified ProcDumpExOption and returns the zero-based index of the first occurrence within the range of elements in the CommandSplitList that extends from the specified index to the last element.
+		/// Searches for the specified ProcDumpExOption and returns the zero-based index of the first occurrence within
+		/// the range of elements in the CommandSplitList that extends from the specified index to the last element.
 		/// </summary>
 		/// <param name="option"></param>
-		/// <returns>The zero-based index of the first occurrence of item within the range of elements in the CommandSplitList that extends from index to the last element, if found; otherwise, -1.</returns>
+		/// <returns>
+		/// The zero-based index of the first occurrence of item within the range of elements in the CommandSplitList
+		/// that extends from index to the last element, if found; otherwise, -1.
+		/// </returns>
 		internal int GetIndexOfOption(string option)
 		{
-			for (int i = 0; i < Count; i++ )
+			for (int i = 0; i < Count; i++)
 			{
 				if (!this[i].IsOption)
+				{
 					continue;
+				}
 
 				//Values with the "IsOption" flag set are always of type string
-				if (this[i].Value.ToString()!.ToLower() == option)
+				if (this[i].Value.ToString() is not { } strValue)
+				{
+					continue;
+				}
+
+				if (strValue.Equals(option, StringComparison.OrdinalIgnoreCase))
+				{
 					return i;
+				}
 			}
 
 			return -1;
 		}
 
+		/// <summary>
+		/// Splits the given command line string into options and values.
+		/// </summary>
+		/// <param name="commandLine"></param>
+		/// <returns></returns>
 		internal static CommandSplitList SplitCommandLineString(string commandLine)
 		{
-			CommandSplitList retTokens = new();
+			CommandSplitList retTokens = [];
 
 			var tokens = commandLine.Split(' ');
-			for (int i = 0; i < tokens.Length; i++)
+
+			int index = 0;
+			while (index < tokens.Length)
 			{
-				string token = tokens[i];
+				string token = tokens[index];
 
 				if (string.IsNullOrEmpty(token))
+				{
+					index++;
 					continue;
+				}
 
 				if (token.StartsWith('-'))
 				{
 					retTokens.Add((IsOption: true, Value: token));
+					index++;
 					continue;
 				}
 
 				if (!token.StartsWith('"'))
 				{
 					retTokens.Add((IsOption: false, Value: token));
+					index++;
 					continue;
 				}
 
 				//Goes through the list until it finds '"' at the end of a value
-				int x = i;
+				int x = index;
 				for (; x < tokens.Length; x++)
 					if (tokens[x].EndsWith('"'))
 						break;
 
 				//Merges all values inside the quotes again and splits them with comma
-				string value = string.Join(" ", tokens[i..(x + 1)]);
+				string value = string.Join(" ", tokens[index..(x + 1)]);
 				retTokens.Add((IsOption: false, Value: value.Trim('"').Split(',').Select(o => o.Trim()).ToList()));
 
-				i = x;
+				index = x;
 			}
 
 			return retTokens;
@@ -64,18 +91,24 @@
 
 		public override string ToString()
 		{
-			List<string> ret = new();
-
+			StringBuilder sb = new();
 
 			foreach (var value in this.Select(o => o.Value))
 			{
 				if (value is List<string> values)
-					ret.Add($"\"{string.Join(',', values)}\"");
+				{
+					sb.Append('"');
+					sb.Append(string.Join(',', values));
+					sb.Append('"');
+				}
 				else
-					ret.Add(value.ToString()!);
+				{
+					sb.Append(value.ToString());
+				}
+				sb.Append(' ');
 			}
 
-			return string.Join(' ', ret);
+			return sb.ToString();
 		}
 	}
 }
